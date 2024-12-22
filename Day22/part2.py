c4ca6584a607e72f8d5ba2618ced028b2e5b2175
MaxMcIntyre <maxmcintyre@gmail.com>
2024-12-22 11:57:36 +0000

    Day 22 readability

--- Day22/part2.py
+++ Day22/part2.py
@@ -7,21 +7,21 @@ all_sequence_prices = dict()
 
 for num in secret_nums:
     curr = num
-    last_digits = [int(str(num)[-1])]
+    prices = [int(str(num)[-1])]
 
     for i in range(2000):
         curr = calculate_next_secret(curr)
-        last_digits.append(int(str(curr)[-1]))
+        prices.append(int(str(curr)[-1]))
     
     price_differences = []
-    for i in range(1, len(last_digits)):
-        price_differences.append(last_digits[i] - last_digits[i-1])
+    for i in range(1, len(prices)):
+        price_differences.append(prices[i] - prices[i-1])
     
     sequences_seen = set()
     for i in range(len(price_differences) - 3):
         sequence = tuple(price_differences[i:i+4])
         if sequence not in sequences_seen:
-            all_sequence_prices[sequence] = all_sequence_prices.get(sequence, 0) + last_digits[i+4]
+            all_sequence_prices[sequence] = all_sequence_prices.get(sequence, 0) + prices[i+4]
             sequences_seen.add(sequence)
 
 max_bananas = max(all_sequence_prices.values())
